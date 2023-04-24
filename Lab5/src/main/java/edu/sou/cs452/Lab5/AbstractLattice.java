f30db11303f1ae9946bba73da18404379cb97a99
alphastigma101 <37643102+alphastigma101@users.noreply.github.com>
2023-04-24 20:00:36 +0000

    This is Lab5

--- Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractLattice.java
+++ Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractLattice.java
@@ -1,5 +1,56 @@
 package edu.sou.cs452.Lab5;
 
+import edu.sou.cs452.Lab5.AbstractValue;
 abstract class AbstractLattic {
+    public final static AbstractValue plus(AbstractValue leftValue, AbstractValue rightValue) {
+        HashMap<AbstractValue, HashMap<AbstractValue, AbstractValue>> lookup = new HashMap<>();
     
+        HashMap<AbstractValue, AbstractValue> left;
+        // left +
+        left = new HashMap<>();
+        left.put(POSITIVE, POSITIVE);
+        left.put(NEGATIVE, TOP);
+        left.put(ZERO, POSITIVE);
+        left.put(BOTTOM, BOTTOM);
+        left.put(TOP, TOP);
+        lookup.put(POSITIVE, left);
+    
+        // left -
+        left = new HashMap<>();
+        left.put(POSITIVE, TOP);
+        left.put(NEGATIVE, NEGATIVE);
+        left.put(ZERO, NEGATIVE);
+        left.put(BOTTOM, BOTTOM);
+        left.put(TOP, TOP);
+        lookup.put(NEGATIVE, left);
+    
+        // left 0
+        left = new HashMap<>();
+        left.put(POSITIVE, POSITIVE);
+        left.put(NEGATIVE, NEGATIVE);
+        left.put(ZERO, ZERO);
+        left.put(BOTTOM, BOTTOM);
+        left.put(TOP, TOP);
+        lookup.put(ZERO, left);
+    
+        // left Bottom
+        left = new HashMap<>();
+        left.put(POSITIVE, BOTTOM);
+        left.put(NEGATIVE, BOTTOM);
+        left.put(ZERO, BOTTOM);
+        left.put(BOTTOM, BOTTOM);
+        left.put(TOP, BOTTOM);
+        lookup.put(BOTTOM, left);
+    
+        // left Top
+        left = new HashMap<>();
+        left.put(POSITIVE, TOP);
+        left.put(NEGATIVE, TOP);
+        left.put(ZERO, TOP);
+        left.put(BOTTOM, BOTTOM);
+        left.put(TOP, TOP);
+        lookup.put(TOP, left);
+    
+        return lookup.get(leftValue).get(rightValue);
+    }
 }
--- Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractValue.java
+++ Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractValue.java
@@ -1,6 +1,12 @@
 package edu.sou.cs452.Lab5;
 
-
+/* 
+ * Part 1: Abstract Values
+ * Create an AbstractValue enum in a file called AbstractValue.java that has the types from the sign lattice. 
+ * The sign lattice has five elements: Top, Positive, Negative, Zero, and Bottom. 
+ * The Top element is the most inclusive element, and the Bottom element is the least inclusive element. 
+ * The Top element represents the set of all possible values, and the Bottom element represents the empty set. The Positive element represents the set of all positive numbers, the Negative element represents the set of all negative numbers, and the Zero element represents the set containing the single number zero.
+*/
 enum AbstractValue {
     BOTTOM, TOP, POSITIVE, NEGATIVE, ZERO, EOF
 }
