40ae50fa7e1b5d0a9b01e9ea993ac3de0f510000
alphastigma101 <37643102+alphastigma101@users.noreply.github.com>
2023-04-24 21:38:37 +0000

    This is Lab5

--- Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractLattice.java
+++ Lab5/src/main/java/edu/sou/cs452/Lab5/AbstractLattice.java
@@ -1,5 +1,6 @@
 package edu.sou.cs452.Lab5;
-// Frzzle is down until openai increases our quota 4/24/23 April 
+// Frazzle is down until openai increases our quota 4/24/23 April 
+// Continue the lab5 without Frazzle 
 import edu.sou.cs452.Lab5.AbstractValue;
 abstract class AbstractLattic {
     public final static AbstractValue plus(AbstractValue leftValue, AbstractValue rightValue) {
